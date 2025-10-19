0b294ff99467626826b1d49cee35fa84caf0dd53
Sibulele Saba <Sibulele.Saba@psg.co.za>
2025-10-19 13:53:13 +0200

    Add project files.

---
+++ RecipeShareApi/Models/Recipe.cs
@@ -0,0 +1,12 @@
+﻿namespace RecipeShareApi.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }   
+        public string Ingredients { get; set; }
+        public string Steps { get; set; }
+        public string CookingTime { get; set; }
+        public string DietaryTag { get; set; }
+    }
+}
