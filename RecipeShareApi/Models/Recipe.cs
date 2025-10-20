cbb3db8a1bae0870c5f3f72271442d44e7445199
Sibulele Saba <Sibulele.Saba@psg.co.za>
2025-10-20 15:20:21 +0200

    ....

--- RecipeShareApi/Models/Recipe.cs
+++ RecipeShareApi/Models/Recipe.cs
@@ -1,12 +1,24 @@
-﻿namespace RecipeShareApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipeShareApi.Models
 {
     public class Recipe
     {
         public int Id { get; set; }
-        public string Title { get; set; }   
+
+        [Required(ErrorMessage = "Title is required.")]
+        [StringLength(100, ErrorMessage = "Title cannot exceed 100 characters")]
+        public string Title { get; set; }
+
+        [MinLength(1, ErrorMessage = "At least one ingredient is required")]
         public List<string> Ingredients { get; set; }
+
+        [Required(ErrorMessage = "Steps are required")]
         public List<string> Steps { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Cooking time must be greater than zero")]
         public string CookingTime { get; set; }
+
         public string DietaryTag { get; set; }
     }
 }
---
+++ RecipeShareApi/Tests/RecipeServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using RecipeShareApi.Data;
+using RecipeShareApi.Services.Implementation;
+
+namespace RecipeShareApi.Tests
+{
+    public class RecipeServiceTests
+    {
+        private readonly RecipeDbContext _recipeDbContext;
+        private readonly RecipeService recipeService;
+
+        public RecipeServiceTests()
+        {
+
+        }
+    }
+}
